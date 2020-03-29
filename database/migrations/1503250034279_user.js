495feb600853bb1ca7c4dc59b2342509d56eaac8
MHenrie <silvamarlon87@gmail.com>
2020-03-29 19:34:36 -0300

    create user schema

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -6,10 +6,12 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.bigInteger('person_code').primary()
+      table.string('name', 254).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('type', 60).notNullable()
+      table.boolean('deleted').defaultTo(false)
       table.timestamps()
     })
   }
